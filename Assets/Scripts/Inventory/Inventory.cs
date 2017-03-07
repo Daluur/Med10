37123a5364d2759f8035e4c1964cd55e429552f4
Mathias Fynbo <matfy87@gmail.com>
2017-03-07 11:38:21 +0100

    - Added inventory / tooltip / itemdatabase script - Added JSON database plugin - Added Resources folder - Temporary UI images

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+	GameObject inventoryPanel;
+	GameObject slotPanel;
+	ItemDatabase database;
+	public GameObject inventorySlot;
+	public GameObject inventoryItem;
+
+	int slotAmount;
+	public List<Item> items = new List<Item> ();
+	public List<GameObject> slots = new List<GameObject> ();
+
+	void Start() {
+		database = GetComponent<ItemDatabase>();
+
+		slotAmount = 12;
+		inventoryPanel = GameObject.Find ("Inventory Panel");
+		slotPanel = inventoryPanel.transform.FindChild ("Slot Panel").gameObject;
+		for (int i = 0; i < slotAmount; i++) {
+			items.Add (new Item());
+			slots.Add (Instantiate (inventorySlot));
+			slots [i].GetComponent<Slot> ().id = i;
+			slots [i].transform.SetParent (slotPanel.transform);
+		}
+
+		AddItem (0);
+		AddItem (1);
+		AddItem (1);
+		AddItem (1);
+		AddItem (1);
+	}
+
+	public void AddItem(int id) {
+		Item itemToAdd = database.FetchItemByID (id);
+
+		if (itemToAdd.Stackable && CheckItemInInventory(itemToAdd)) {
+			for (int i = 0; i < items.Count; i++) {
+				if (items[i].ID == id) {
+					ItemData data = slots [i].transform.GetChild (0).GetComponent<ItemData> ();
+					data.amount++;
+					data.transform.GetChild (0).GetComponent<Text> ().text = data.amount.ToString ();
+
+					break;
+				}
+			}
+		}
+
+		else {
+			for (int i = 0; i < items.Count; i++) {
+				if (items[i].ID == -1) {
+					items [i] = itemToAdd;
+					GameObject itemObject = Instantiate (inventoryItem);
+					itemObject.GetComponent<ItemData> ().item = itemToAdd;
+					itemObject.GetComponent<ItemData> ().slot = i;
+					itemObject.transform.SetParent (slots[i].transform);
+					itemObject.transform.position = Vector2.zero;
+					itemObject.GetComponent<Image> ().sprite = itemToAdd.Sprite;
+					itemObject.name = itemToAdd.Title;
+					ItemData data = slots[i].transform.GetChild (0).GetComponent<ItemData> ();
+					data.amount = 1;
+					break;
+				}
+			}
+		}
+	}
+
+	bool CheckItemInInventory(Item item) {
+		for (int i = 0; i < items.Count; i++) {
+			if (items[i].ID == item.ID) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ Assets/Scripts/Inventory/Tooltip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour {
+	private Item item;
+	private string data;
+	private GameObject tooltip;
+
+	void Start() {
+		tooltip = GameObject.Find ("Tooltip");
+		tooltip.SetActive (false);
+	}
+
+	void Update() {
+		if (tooltip.activeSelf) {
+			tooltip.transform.position = Input.mousePosition;
+		}
+	}
+
+	public void Activate(Item item) {
+		this.item = item;
+		ConstructData ();
+		tooltip.SetActive (true);
+	}
+
+	public void Deactivate() {
+		tooltip.SetActive (false);
+	}
+
+	public void ConstructData() {
+		data = "<color=#011100><b>" + item.Title + "</b></color>\n\n" + item.Description + "\n\nType: " + item.Type;
+		tooltip.transform.GetChild (0).GetComponent<Text> ().text = data;
+	}
+
+}
