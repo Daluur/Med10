d2c1b680741ee87304708472261be774f04e4987
Daluur <fknudsen@gmail.com>
2017-03-21 13:19:15 +0100

    Created a light on each unit. The lights color will be corrosponding the unit's type.

--- Assets/Scripts/CombatWorld/Units/Unit.cs
+++ Assets/Scripts/CombatWorld/Units/Unit.cs
@@ -12,10 +12,9 @@ namespace CombatWorld.Units {
 		public string attackName = "Melee Right Attack 01";
 		public GameObject projectile;
 
-		[SerializeField]
-		private bool shadowUnit = false;
+		public Light lightSource;
 
-		[SerializeField]
+		private bool shadowUnit = false;
 		private bool stoneUnit = false;
 		bool turnedToStone = false;
 
@@ -35,7 +34,6 @@ namespace CombatWorld.Units {
 		private Vector3 defaultFaceDirection;
 
 		private float moveSpeed = 12.5f;
-
 
 		private AnimationHandler animHelp;
 
@@ -202,13 +200,30 @@ namespace CombatWorld.Units {
 			stoneUnit = data.stone;
 			shadowUnit = data.shadow;
 			node.SetOccupant(this);
-			if(team == Team.Player) {
+			lightSource.color = GetColorFromType();
+			if (team == Team.Player) {
 				defaultFaceDirection = Vector3.right;
 			}
 			else {
 				defaultFaceDirection = Vector3.left;
 			}
 			FaceForward();
+		}
+
+		Color GetColorFromType() {
+			switch (type) {
+				case ElementalTypes.Fire:
+					return Color.red;
+				case ElementalTypes.Water:
+					return Color.blue;
+				case ElementalTypes.Nature:
+					return Color.green;
+				case ElementalTypes.Lightning:
+					return Color.yellow;
+				case ElementalTypes.NONE:
+				default:
+					return Color.white;
+			}
 		}
 
 		IEnumerator MoveTo(List<Node> target) {
