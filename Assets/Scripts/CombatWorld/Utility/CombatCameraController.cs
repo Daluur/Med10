2be44f878c18209f24bca382e730a6662a15d23e
Daluur <fknudsen@gmail.com>
2017-03-27 15:49:27 +0200

    minor changes to map. and cam controller

--- Assets/Scripts/CombatWorld/Utility/CombatCameraController.cs
+++ Assets/Scripts/CombatWorld/Utility/CombatCameraController.cs
@@ -4,11 +4,15 @@ namespace CombatWorld {
 	public class CombatCameraController : MonoBehaviour {
 
 		float speed = 60.0f;
+		Vector3 newPos;
+		Vector2 minMaxXPos = new Vector2(-40, 40);
 
 		void LateUpdate() {
 			if (Input.GetMouseButton(0)) {
 				if (Input.GetAxis("Mouse X") != 0) {
-					transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, 0, 0);
+					newPos = transform.position - new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, 0, 0);
+					newPos.x = Mathf.Clamp(newPos.x, minMaxXPos.x, minMaxXPos.y);
+					transform.position = newPos;
 				}
 			}
 		}
