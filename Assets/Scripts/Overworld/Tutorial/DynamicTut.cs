1248a7e676dd49ab76e3822f6d4054b5d82131b8
Bo <bolacour@hotmail.com>
2017-05-01 16:35:20 +0200

    Added dynamic tuts based on data gathering

--- Assets/Scripts/Overworld/Tutorial/DynamicTut.cs
+++ Assets/Scripts/Overworld/Tutorial/DynamicTut.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using CombatWorld;
+using CombatWorld.Utility;
+using NUnit.Framework.Internal;
 using Overworld;
 using UnityEngine;
 
-public class DynamicTut : MonoBehaviour {
+public class DynamicTut : Singleton<DynamicTut> {
 
 	public bool isDynamic;
 	private string textToShow = "", buttonOne = "Okay";
 	public float moveDelay = 3f, endTurnDelay = 4f, playerSelectionDelay = 4f;
 
 	private bool showGoalAndSummon, summonSickness, playerMovement, winning, losing, playerAttacking, endTurn;
+
+	private bool inCombat, shownType, shownRetaliation;
+	private Coroutine testing;
+	public float cooldownForDynamicTypeTut = 100f, cooldownForDynamicRetaliationTut = 100f;
+
 
 	//TODO: Add all the different types of tuts we want to be shown in here
 
@@ -28,6 +36,102 @@ public class DynamicTut : MonoBehaviour {
 
 
 	void Update () {
+	}
+
+
+	private void StartTestingForDynamicOccurences() {
+		testing = StartCoroutine(Testing());
+	}
+
+	private void ForceStopTestingForDynamicOccurences() {
+		if(testing!=null)
+			StopCoroutine(testing);
+	}
+
+	IEnumerator Testing() {
+		Debug.Log("Startedtesting");
+		while(inCombat){
+			yield return new WaitForSeconds(3f);
+			ShowDynamicTypes();
+		}
+		Debug.Log("Stopped testing");
+	}
+
+	IEnumerator TypesCooldown() {
+		yield return new WaitForSeconds(cooldownForDynamicTypeTut);
+		shownType = false;
+	}
+	IEnumerator RetaliationCooldown() {
+		yield return new WaitForSeconds(cooldownForDynamicRetaliationTut);
+		shownRetaliation = false;
+	}
+
+
+	public void ShowDynamicTypes() {
+		if(shownType)
+			return;
+
+		var playerTrades = GetPlayerTrades();
+		var aiTrades = GetAITrades();
+		if(playerTrades.Count == 0 || aiTrades.Count == 0)
+			return;
+
+		StartCoroutine(TypesCooldown());
+
+		FilterTowerAttacks(ref playerTrades);
+		FilterTowerAttacks(ref aiTrades);
+
+		var score = playerTrades.FindAll(element => element.good).Count - playerTrades.FindAll(element => element.bad).Count;
+		var aiScore =  aiTrades.FindAll(element => element.good).Count - aiTrades.FindAll(element => element.bad).Count;
+
+		//TODO: Understand which type of bad attack triggered this and use that, or use general knowledge?
+		var atk = playerTrades.Find(element => element.bad).attacker;
+		var def = playerTrades.Find(element => element.bad).defender;
+
+		if(score < -2 && aiScore > 1){
+			Debug.Log("SHOW THE ELEMENTALTYPE STUFF HERE! LOOK AT TODO IN THIS FUNCTION");
+			GeneralConfirmationBox.instance.ShowPopUp(
+				String.Format("Type {0} is weak against {1} \n Maybe you should try using a different type of unit", atk, def),"Okay");
+		}
+	}
+
+	public void ShowRetaliationDynamic() {
+		if(shownRetaliation)
+			return;
+
+		var playerTrades = GetPlayerTrades();
+		var aiTrades = GetAITrades();
+		if(playerTrades.Count == 0 || aiTrades.Count == 0)
+			return;
+
+		StartCoroutine(RetaliationCooldown());
+
+		FilterTowerAttacks(ref playerTrades);
+		FilterTowerAttacks(ref aiTrades);
+
+		var score = (playerTrades.Count - playerTrades.FindAll(element => element.killHit).Count) - (aiTrades.Count - aiTrades.FindAll(element => element.killHit).Count);
+
+		if (score < 0) {
+			Debug.Log("THE PLAYER NEEDS TO BE TAUGHT ABOUT RETALIATION");
+			GeneralConfirmationBox.instance.ShowPopUp("If a unit survives an attack\n It will retaliate","Okay");
+		}
+	}
+
+	private void FilterTowerAttacks(ref List<CombatTrades> trades) {
+		trades.RemoveAll(element => element.towerHit);
+	}
+
+	private List<CombatTrades> GetPlayerTrades() {
+		return DataGathering.Instance.GetAllTrades().FindAll(element => element.initiator == Team.Player);
+	}
+	private List<CombatTrades> GetAITrades() {
+		return DataGathering.Instance.GetAllTrades().FindAll(element => element.initiator == Team.AI);
+	}
+
+	public void SetCombat(bool value) {
+		inCombat = value;
+		if (inCombat)
+			StartTestingForDynamicOccurences();
 	}
 
 
